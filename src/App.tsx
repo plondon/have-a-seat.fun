3f9f49c1c9fdfbbb995f5413a2a9a37d1db028be
Philip London <philip@blockchain.com>
2019-12-21 09:51:40 -0500

    💌 Add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import styled from 'styled-components';
 // Import css files
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 // Import photos
+import AboutImg from './images/About.jpg';
 import ConeyIsland from './images/Coney Island, 2016.jpg';
 import KyotoRestaurantBench from './images/Kyoto Restaurant Bench, 2018.jpg';
 import Lanzarote from './images/Lanzarote, 2018.jpg';
-import LisbonBusStop from './images/Lisbon Bus Stop, 2019.jpg';
 import LisbonOverlookWithBench from './images/Lisbon Overlook with Bench, 2019.jpg';
-import LisbonParkBench from './images/Lisbon Park Bench, 2019.jpg';
 import LisbonParkBench2 from './images/Lisbon Park Bench 2, 2019.jpg';
 import LisbonParkBench3 from './images/Lisbon Park Bench 3, 2019.jpg';
-import LisbonParkBench4 from './images/Lisbon Park Bench 4, 2019.jpg';
 import LisbonParkBenchOcean from './images/Lisbon Park Bench by the Ocean, 2019.jpg';
 import LisbonTrainStation from './images/Lisbon Train Station, 2019.jpg';
 import LosJardinesLaGranja from './images/Los Jardines de La Granja de San Ildefonso, 2019.jpg';
@@ -60,6 +58,14 @@ const Container = styled.div`
   color: #ffffff;
   height: 100%;
 `;
+const MainContent = styled.div`
+  height: 100%;
+  filter: blur(0px);
+  transition: filter 0.7s;
+  &.ABOUT-ACTIVE {
+    filter: blur(5px);
+  }
+`;
 const CustomSlider = styled(Slider)`
   height: calc(100% - 5rem);
   .slick-list,
@@ -100,167 +106,185 @@ const SlideInfo = styled.div`
     margin-top: 0.5rem;
   }
 `;
+const About = styled.div`
+  background: #101010;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 2rem;
+  position: absolute;
+  bottom: -385px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  border-top: 1px solid #2d2d2d;
+  transition: bottom 0.3s 0s;
+  img {
+    margin-right: 1rem;
+    height: 20rem;
+  }
+  &.ABOUT-ACTIVE {
+    bottom: 0px;
+    transition: bottom 0.3s 0.3s;
+  }
+  @media (max-width: 767px) {
+    flex-direction: column;
+    bottom: -410px;
+    img {
+      margin-bottom: 1rem;
+    }
+  }
+`;
+const AboutButton = styled.div`
+  cursor: pointer;
+  position: fixed;
+  bottom: 3rem;
+  right: 3rem;
+  z-index: 1;
+  @media (max-width: 767px) {
+    bottom: 1rem;
+    right: 1rem;
+  }
+`;
 
 const App: React.FC = () => {
+  const [isAboutActive, setIsAboutActive] = useState(false);
+
   return (
     <Container>
-      <h1 style={{ textAlign: 'center' }}>🛋 have a seat</h1>
-      <CustomSlider {...SETTINGS}>
-        <SlideContainer>
-          <img src={LisbonTrainStation} alt="lisbon" />
-          <SlideInfo>
-            <h3>Lisbon Train Station</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={ParisTrainStation2} alt="paris train station" />
-          <SlideInfo>
-            <h3>Paris Train Station</h3>
-            <h6>Paris, France 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={MadridParkBench2} alt="lisbon" />
-          <SlideInfo>
-            <h3>Madrid Park Bench</h3>
-            <h6>Madrid, Spain 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={ConeyIsland} alt="coney island" />
-          <SlideInfo>
-            <h3>Coney Island</h3>
-            <h6>Coney Island, New York 2016</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={MadridParkBench} alt="madrid park bench" />
-          <SlideInfo>
-            <h3>Madrid Park Bench</h3>
-            <h6>Madrid, Spain 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonParkBenchOcean} alt="lisbon park bench ocean" />
-          <SlideInfo>
-            <h3>Lisbon Park Bench by the Ocean</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={KyotoRestaurantBench} alt="kyoto bench" />
-          <SlideInfo>
-            <h3>Kyoto Restaurant Bench</h3>
-            <h6>Kyoto, Japan 2018</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={Lanzarote} alt="lanzarote bench" />
-          <SlideInfo>
-            <h3>Césear Manrique House</h3>
-            <h6>Lanzarote, Spain 2018</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LosJardinesLaGranja} alt="la granja" />
-          <SlideInfo>
-            <h3>Los Jardines de La Granja San Ildefonso</h3>
-            <h6>La Granja, Spain 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={Mundaka} alt="mundaka" />
-          <SlideInfo>
-            <h3>Mundaka</h3>
-            <h6>Mundaka, Spain 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={MadridRedBench} alt="madrid red bench" />
-          <SlideInfo>
-            <h3>Madrid Red Bench</h3>
-            <h6>Madrid, Spain 2017</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={Skiathos} alt="skiathos" />
-          <SlideInfo>
-            <h3>Skiathos Bench</h3>
-            <h6>Skiathos, Greece 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={TokyoSubway} alt="tokyo" />
-          <SlideInfo>
-            <h3>Tokyo Green Subway Seat</h3>
-            <h6>Tokyo, Japan 2017</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={ToledoTrainStation} alt="toledo" />
-          <SlideInfo>
-            <h3>Toledo Train Station</h3>
-            <h6>Toledo, Spain 2017</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonParkBench} alt="lisbon" />
-          <SlideInfo>
-            <h3>Lisbon Park Bench</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonBusStop} alt="lisbon bus stop" />
-          <SlideInfo>
-            <h3>Lisbon Bus Stop</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonParkBench2} alt="lisbon park bench" />
-          <SlideInfo>
-            <h3>Lisbon Park Bench</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonOverlookWithBench} alt="lisbon overlook" />
-          <SlideInfo>
-            <h3>Lisbon Overlook with Bench</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonParkBench3} alt="lisbon park bench" />
-          <SlideInfo>
-            <h3>Lisbon Park Bench</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={LisbonParkBench4} alt="lisbon park bench" />
-          <SlideInfo>
-            <h3>Lisbon Park Bench</h3>
-            <h6>Lisbon, Portugal 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={ParisTrainStation} alt="paris train station" />
-          <SlideInfo>
-            <h3>Paris Train Station</h3>
-            <h6>Paris, France 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-        <SlideContainer>
-          <img src={ParisTrainStation3} alt="paris train station" />
-          <SlideInfo>
-            <h3>Paris Train Station</h3>
-            <h6>Paris, France 2019</h6>
-          </SlideInfo>
-        </SlideContainer>
-      </CustomSlider>
+      <MainContent className={isAboutActive ? 'ABOUT-ACTIVE' : ''}>
+        <h1 style={{ textAlign: 'center' }}>🛋 have a seat</h1>
+        <CustomSlider {...SETTINGS}>
+          <SlideContainer>
+            <img src={LisbonTrainStation} alt="lisbon" />
+            <SlideInfo>
+              <h3>Lisbon, Portugal 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={MadridParkBench2} alt="lisbon" />
+            <SlideInfo>
+              <h3>Madrid, Spain 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={ParisTrainStation2} alt="paris train station" />
+            <SlideInfo>
+              <h3>Paris, France 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={ConeyIsland} alt="coney island" />
+            <SlideInfo>
+              <h3>Coney Island, New York 2016</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={MadridParkBench} alt="madrid park bench" />
+            <SlideInfo>
+              <h3>Madrid, Spain 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={LisbonParkBenchOcean} alt="lisbon park bench ocean" />
+            <SlideInfo>
+              <h3>Lisbon, Portugal 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={KyotoRestaurantBench} alt="kyoto bench" />
+            <SlideInfo>
+              <h3>Kyoto, Japan 2018</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={Lanzarote} alt="lanzarote bench" />
+            <SlideInfo>
+              <h3>Lanzarote, Spain 2018</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={LosJardinesLaGranja} alt="la granja" />
+            <SlideInfo>
+              <h3>La Granja, Spain 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={Mundaka} alt="mundaka" />
+            <SlideInfo>
+              <h3>Mundaka, Spain 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={MadridRedBench} alt="madrid red bench" />
+            <SlideInfo>
+              <h3>Madrid, Spain 2017</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={Skiathos} alt="skiathos" />
+            <SlideInfo>
+              <h3>Skiathos, Greece 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={TokyoSubway} alt="tokyo" />
+            <SlideInfo>
+              <h3>Tokyo, Japan 2017</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={ToledoTrainStation} alt="toledo" />
+            <SlideInfo>
+              <h3>Toledo, Spain 2017</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={LisbonParkBench2} alt="lisbon park bench" />
+            <SlideInfo>
+              <h3>Lisbon, Portugal 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={LisbonOverlookWithBench} alt="lisbon overlook" />
+            <SlideInfo>
+              <h3>Lisbon, Portugal 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={LisbonParkBench3} alt="lisbon park bench" />
+            <SlideInfo>
+              <h3>Lisbon, Portugal 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={ParisTrainStation} alt="paris train station" />
+            <SlideInfo>
+              <h3>Paris, France 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+          <SlideContainer>
+            <img src={ParisTrainStation3} alt="paris train station" />
+            <SlideInfo>
+              <h3>Paris, France 2019</h3>
+            </SlideInfo>
+          </SlideContainer>
+        </CustomSlider>
+      </MainContent>
+      <AboutButton onClick={() => setIsAboutActive(!isAboutActive)}>
+        <span role="img" aria-label="envelope" style={{ fontSize: '2rem' }}>
+          💌
+        </span>
+      </AboutButton>
+      <About className={isAboutActive ? 'ABOUT-ACTIVE' : ''}>
+        <img src={AboutImg} alt="about" />
+        <div>
+          Sina, when the four of us were in Athens together you told me that you
+          loved the different personalities of benches from different cities. It
+          was our last conversation before Lola and I moved. I decided to make a
+          collection of benches from the places we've been to together, places
+          from recent adventures, and places I hope you get to visit.
+        </div>
+      </About>
     </Container>
   );
 };
